d374f712a5caf42fadc37a35e03490a5336266b9
Riyaz <riyazpasha880@gmail.com>
2022-07-04 23:51:19 +0530

    Populate 1M records in the table

---
+++ 3_Populate_Table.sql
@@ -0,0 +1,14 @@
+SET NOCOUNT ON
+DECLARE @counter int=1
+WHILE (@counter <= 1000000)
+    BEGIN
+        DECLARE @Name varchar(50)='Name' + RTRIM(@counter)
+        DECLARE @Email varchar(255)='email' + RTRIM(@counter) + '@mail.com'
+
+        INSERT INTO Employees VALUES (@Name, @Email)
+
+        SET @counter = @counter + 1
+
+        IF (@counter % 100000 = 0)
+            PRINT RTRIM(@counter) + ' rows inserted'
+    END
