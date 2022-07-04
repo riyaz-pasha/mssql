a7a8930043f4704982143802254b47c046d78a5d
Riyaz <riyazpasha880@gmail.com>
2022-07-04 23:44:26 +0530

    Create table in master database

---
+++ 2_Create_Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees
+(
+    id    int primary key identity,
+    name  varchar(50),
+    email varchar(255)
+);
+
+-- View tables
+-- SELECT *
+-- FROM INFORMATION_SCHEMA.TABLES;
